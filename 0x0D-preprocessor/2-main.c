3b3d1064903429d3bf94d6ff2fcaa18ad4b0a9fb
femilana <phemeelana@gmail.com>
2023-09-17 06:18:18 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
- * Return: Always 0 (Success)
+ * main -Entry point that  prints the name of the file
+ *Return: 0
  */
 int main(void)
 {
